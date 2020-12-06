93191e8ff8928e54de88802c59bc506cd8c15d7a
mateuscesar <mateus.cesar@egov.com>
2020-12-06 14:52:22 -0500

    Added cards and chart

---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,17 @@
+import React, {useState,useEffect} from 'react';
+import {NativeSelect,FormControl} from '@material-ui/core'
+
+import styles from './CountryPicker.module.css'
+
+const CountryPicker = () => {
+    return (
+        <FormControl className={styles.formControl}>
+            <NativeSelect>
+                <option value="global">Global</option>
+            </NativeSelect>
+
+        </FormControl>
+    )
+}
+
+export default CountryPicker;
